d3f6559e8374db5b47cd1c7096adea295090b5b2
JonathanFerreira10 <johnnybravo.jc@hotmail.com>
2023-10-13 12:33:17 -0300

    test

--- src/android/com/adobe/phonegap/push/BackgroundActionButtonHandler.java
+++ src/android/com/adobe/phonegap/push/BackgroundActionButtonHandler.java
@@ -40,6 +40,7 @@ public class BackgroundActionButtonHandler extends BroadcastReceiver implements 
             }
 
             PushPlugin.sendExtras(originalExtras);
+            //teste
         }
      }
 }
